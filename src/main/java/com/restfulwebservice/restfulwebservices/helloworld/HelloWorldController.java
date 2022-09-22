b0515c55c978fe41e94f44697e1a32a9810e2075
12Ramji34@& <pallavidubey0823@gmail.com>
2022-09-22 14:09:04 +0530

    GetMapping annotation is more readable then RequestMapping

--- src/main/java/com/restfulwebservice/restfulwebservices/helloworld/HelloWorldController.java
+++ src/main/java/com/restfulwebservice/restfulwebservices/helloworld/HelloWorldController.java
@@ -1,12 +1,13 @@
 package com.restfulwebservice.restfulwebservices.helloworld;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloWorldController {
-@RequestMapping(method = RequestMethod.GET,path = "/hello_world")
+@GetMapping(path = "/hello_world")
     public String hellWorld() {
         return "Hello World";
     }
