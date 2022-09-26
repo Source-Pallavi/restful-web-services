5957c3a7d39d6cc53353463494dd63aa644f90db
12Ramji34@& <pallavidubey0823@gmail.com>
2022-09-26 15:41:43 +0530

    Mapping using path variable

--- src/main/java/com/restfulwebservice/restfulwebservices/helloworld/HelloWorldController.java
+++ src/main/java/com/restfulwebservice/restfulwebservices/helloworld/HelloWorldController.java
@@ -1,9 +1,6 @@
 package com.restfulwebservice.restfulwebservices.helloworld;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class HelloWorldController {
@@ -14,5 +11,9 @@ public class HelloWorldController {
     @GetMapping(path = "/hello_worldbean")
     public HelloWorldBean hellWorldBean() {//how to get response in format of json
         return new HelloWorldBean("Hello World");
+    }
+    @GetMapping(path = "/hello_worldpathformatter/{name}")
+    public HelloWorldBean hellWorldBeanPathVariable(@PathVariable String name) {
+        return new HelloWorldBean(String.format("Hello World %s",name));
     }
 }
